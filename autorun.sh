7ab87a19a379f50bab37edc77fd62db600d33ee5
Wei Xing <wayne.xingle@gmail.com>
2024-01-22 00:44:51 +0000

    update

--- autorun.sh
+++ autorun.sh
@@ -4,9 +4,14 @@
 conda info --envs
 
 # source activate python27
+# install: conda create -n py27 python=2.7
+
+conda activate py27
 echo python --version
+
 python jemdoc.py index.jemdoc
 
 # source deactivate python27
 
 echo 'Finish compling index.jemdoc'
+conda deactivate py27
